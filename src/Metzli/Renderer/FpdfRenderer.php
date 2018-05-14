f01d1ba6b53d3b59b7594c1e3b7b63c5a4b50457
Erwin de Haan <EraYaN@users.noreply.github.com>
2018-05-14 16:37:19 +0900

    Removed superfluos setters/methods and addressed other feedback.

--- src/Metzli/Renderer/FpdfRenderer.php
+++ src/Metzli/Renderer/FpdfRenderer.php
@@ -31,7 +31,7 @@ class FpdfRenderer implements RendererInterface
 
     private $s;
 
-    public function __construct(\FPDF $fpdfLikeObject = null, $s = false, $x = false, $y = false, $fgColor = array(0, 0, 0), $bgColor = array(256, 256, 256))
+    public function __construct(\FPDF $fpdfLikeObject, $s, $x, $y, $fgColor = array(0, 0, 0), $bgColor = null)
     {
         $this->fgColor = $fgColor;
         $this->bgColor = $bgColor;
@@ -40,40 +40,14 @@ class FpdfRenderer implements RendererInterface
         $this->y = $y;
         $this->s = $s;
     }
-    public function setFPDF(FPDF $fpdfLikeObject){
-        $this->pdf = $fpdfLikeObject;
-    }
-    public function setSizeAndOffset($s,$x,$y){
-        $this->x = $x;
-        $this->y = $y;
-        $this->s = $s;
-    }
-    private function parseColor($color)
-    {
-        if (!is_array($color) || count($color) != 3) {
-            throw new \InvalidArgumentException('Color array does not have three components');
-        }
-        for ($i = 0; $i < count($color); $i++) {
-            $color[$i] = intval($color[$i]);
-        }
-        return $color;
-    }
 
     public function render(AztecCode $code)
-    {
-        if(!$this->pdf){
-            throw new \InvalidArgumentException('FPDF was not set yet. Please set the FPDF object using setFPDF() or in the constructor.');
-        }
-        if($this->x===false||$this->y===false||$this->s===false){
-            throw new \InvalidArgumentException('Size and offsets were not set yet. Please set the size and offsets using setSizeAndOffset() or in the constructor.');
-        }
+    {        
         $matrix = $code->getMatrix();       
-        $fg = $this->parseColor($this->fgColor);
-        $bg = $this->parseColor($this->bgColor);
 
-        if($bg[0]<256&&$bg[1]<256&&$bg[2]<256){
+        if($this->bgColor!=null){
             // Background
-            $this->pdf->SetFillColor($bg[0],$bg[1],$bg[2]);
+            $this->pdf->SetFillColor($this->bgColor[0],$this->bgColor[1],$this->bgColor[2]);
             // Make background
             $this->pdf->Rect($this->x, $this->y, $this->s, $this->s, 'F' );
         }
@@ -82,7 +56,7 @@ class FpdfRenderer implements RendererInterface
         $cellsize_h = $this->s/$matrix->getHeight();
 
         // Foreground
-        $this->pdf->SetFillColor($fg[0],$fg[1],$fg[2]);
+        $this->pdf->SetFillColor($this->fgColor[0],$this->fgColor[1],$this->fgColor[2]);
 
         for ($x = 0; $x < $matrix->getWidth(); $x++) {
             for ($y = 0; $y < $matrix->getHeight(); $y++) {
@@ -90,7 +64,6 @@ class FpdfRenderer implements RendererInterface
                     $this->pdf->Rect($this->x+$x*$cellsize_w, $this->y+$y*$cellsize_h, $cellsize_w, $cellsize_h, 'F' );
                 }
             }
-        }
-        return true;       
+        }      
     }
 }
